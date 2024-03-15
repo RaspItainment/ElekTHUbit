e115963e36ffb29e09f5807fa6805967d0257cc9
Fabian Lippold <fabianlippold1184@gmail.com>
2024-03-15 17:50:45 +0100

    update

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # ElekTHUbit
 
-```bash
-ssh debian@<ip> -- sudo apt update --yes && sudo apt install git --yes && git clone https://github.com/RaspItainment/ElekTHUbit.git && cd ElekTHUbit/ && ./setup/setup.sh
-```
-
 Teamprojekt with Elektrobit
---
+++ raspberry-vanilla/build/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+repo init -u https://android.googlesource.com/platform/manifest -b android-14.0.0_r22 --depth=1
+
+mkdir -p .repo/local_manifests
+manifest_brcm_rpi.xml remove_projects.xml .repo/local_manifests/
+
+repo sync -c -j 8
+
+. build/envsetup.sh
+
+lunch aosp_rpi4_car-userdebug
+
+make bootimage systemimage vendorimage -j 8
+
+./rpi4-mkimg.sh
