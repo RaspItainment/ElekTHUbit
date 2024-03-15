b0d4f55d6d72e1fd673cce0c578984c0093d0aa9
Fabian Lippold <fabianlippold1184@gmail.com>
2024-03-15 19:54:23 +0100

    update

--- raspberry-vanilla/build/build.sh
+++ raspberry-vanilla/build/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -v
+set -e
+
 repo init -u https://android.googlesource.com/platform/manifest -b android-14.0.0_r22 --depth=1
 
 mkdir -p .repo/local_manifests
-manifest_brcm_rpi.xml remove_projects.xml .repo/local_manifests/
+cp manifest_brcm_rpi.xml remove_projects.xml .repo/local_manifests/
 
 repo sync -c -j 8
 
